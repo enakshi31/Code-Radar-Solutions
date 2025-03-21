ebec42298639c6fa6cbf03e55b8bcb1604c9042f
enakshi31 <enakshi0148@gmail.com>
2025-03-21 15:34:30 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,68 +1,32 @@
-// #include<stdio.h>
-// void bubbleSort(int arr[], int n){
-//     for (int i=0;i<n-1;i++){
-//         for (int j=0;j<n-1;j++){
-//             if (arr[j]>arr[j+1]){
-//                 int temp = arr[j];
-//                 arr[j] = arr[j+1];
-//                 arr[j+1] = temp;
-//             }
-//         }
-//     }
-// }
-
-// void printArray (int arr[], int n) {
-//     for (int i=0;i<n;i++){
-//         printf("%d",arr[i]);
-//     }
-//     printf("\n");
-// }
-
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
-//     }
-//     bubbleSort(arr,n);
-//     printArray(arr,n);
-//     return 0;
-// }
-
-
-
-
-#include <stdio.h>
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+#include<stdio.h>
+void bubbleSort(int arr[], int n){
+    for (int i=0;i<n-1;i++){
+        for (int j=0;j<n-1;j++){
+            if (arr[j]>arr[j+1]){
                 int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
 }
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+
+void printArray (int arr[], int n) {
+    for (int i=0;i<n;i++){
+        printf("%d",arr[i]);
     }
     printf("\n");
 }
-int main() {
+
+int main(){
     int n;
-    scanf("%d", &n);
+    scanf("%d",&n);
     int arr[n];
-    
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    bubbleSort(arr, n);
-    printArray(arr, n);
-
+    bubbleSort(arr,n);
+    printArray(arr,n);
     return 0;
 }
 
